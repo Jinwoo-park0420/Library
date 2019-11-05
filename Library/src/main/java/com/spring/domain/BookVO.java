e279199225daeba6742372a84321321e34a0c372
jeheon <lvlkh@naver.com>
2019-11-05 15:02:16 +0900

    BookVO 수정

--- Library/src/main/java/com/spring/domain/BookVO.java
+++ Library/src/main/java/com/spring/domain/BookVO.java
@@ -3,10 +3,10 @@ package com.spring.domain;
 import java.util.Date;
 
 public class BookVO {
-	private String b_code; //도서번호
-	private String b_name; //도서이름
-	private String b_genre; //도서장르(카테고리)
-	private String b_writer; //도서저자
-	private Date newbookdate; //신간도서 날짜
-	private Date lenbookdate; //대여 날짜
+	private String Library_bookcode; //도서번호
+	private String Library_bookname; //도서이름
+	private String Library_bookgenre; //도서장르(카테고리)
+	private String Library_bookwriter; //도서저자
+	private Date Library_newbookdate; //신간도서 날짜
+	private Date Library_lenbookdate; //대여 날짜
 }
