d6f09df12c5a778f535b61c6a34f3fa434e2742f
jeheon <lvlkh@naver.com>
2019-11-05 15:10:35 +0900

    Library_book DB

---
+++ Library/Library_book.sql
@@ -0,0 +1,8 @@
+create table Library_book(
+	Library_bookcode nvarchar2(10) not null,
+	Library_bookname nvarchar2(100) not null,
+	Library_bookgenre nvarchar2(20) not null,
+	Library_bookwriter nvarchar2(50) not null,
+	Library_newbookdate date default sysdate,
+	Library_lenbookdate date default sysdate
+);
