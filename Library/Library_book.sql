6716211b65284819ee8a688da9ccb2d34b7dc063
chankyung <champ356@naver.com>
2019-11-05 17:00:50 +0900

    Library_bookstatus 추가

--- Library/Library_book.sql
+++ Library/Library_book.sql
@@ -3,6 +3,7 @@ create table Library_book(
 	Library_bookname nvarchar2(100) not null,
 	Library_bookgenre nvarchar2(20) not null,
 	Library_bookwriter nvarchar2(50) not null,
+	Library_bookstatus nvarchar2(10) not null,
 	Library_newbookdate date default sysdate,
 	Library_lenbookdate date default sysdate
 );
--- Library/src/main/java/com/spring/domain/BookVO.java
+++ Library/src/main/java/com/spring/domain/BookVO.java
@@ -9,4 +9,5 @@ public class BookVO {
 	private String Library_bookwriter; //도서저자
 	private Date Library_newbookdate; //신간도서 날짜
 	private Date Library_lenbookdate; //대여 날짜
+	private String Library_bookstatus; //대여 상태 (대여가능 or 대여 중)
 }
