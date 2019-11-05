5edcf4d9c842cb3e5b234714a75a3d2174be8aa9
rotess <chanhae1020@gmail.com>
2019-11-05 14:38:39 +0900

    게시판 sql

---
+++ Library/src/main/java/com/spring/domain/BoardVO.java
@@ -0,0 +1,15 @@
+package com.spring.domain;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+
+	private int bno;
+	private String title;
+	private Date regdate;
+	private Date updatedate;
+	private int readcnt;
+}
